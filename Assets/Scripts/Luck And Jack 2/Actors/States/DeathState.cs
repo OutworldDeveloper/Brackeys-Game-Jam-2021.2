c2edc1afcb1f37bfb25f642ca39ce34ab19a77d6
Malik <malik.muhametchin@gmail.com>
2021-08-27 00:57:29 +0200

    sort scripts

--- Assets/Scripts/Luck And Jack 2/Actors/States/DeathState.cs
+++ Assets/Scripts/Luck And Jack 2/Actors/States/DeathState.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.AI;
 
 public class DeathState : State
 {
@@ -15,45 +14,6 @@ public class DeathState : State
     public override void Start()
     {
         _animator.SetTrigger(DeathTrigger);
-    }
-
-}
-
-public class RatDeathState : DeathState
-{
-
-    private const float KnockbackDuration = 0.2f;
-    private const float KnockbackForce = 12f;
-
-    private readonly Rat _rat;
-    private readonly NavMeshAgent _agent;
-    private readonly RotationController _rotationController;
-
-    private float _endTime;
-
-    public RatDeathState(Rat rat, NavMeshAgent agent, RotationController rotationController, Animator animator) : base(animator)
-    {
-        _rat = rat;
-        _agent = agent;
-        _rotationController = rotationController;
-    }
-
-    public override void Start()
-    {
-        base.Start();
-        _endTime = Time.time + KnockbackDuration;
-        _agent.ResetPath();
-        _agent.isStopped = true;
-        _rotationController.LookIn(-_rat.LastDamageDirection);
-    }
-
-    public override void Tick()
-    {
-        base.Tick();
-        if (Time.time < _endTime)
-        {
-            _rat.transform.position += _rat.LastDamageDirection * KnockbackForce * Time.deltaTime;
-        }
     }
 
 }
---
+++ Assets/Scripts/Luck And Jack 2/Actors/States/RatDeathState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class RatDeathState : DeathState
+{
+
+    private const float KnockbackDuration = 0.2f;
+    private const float KnockbackForce = 12f;
+
+    private readonly Rat _rat;
+    private readonly NavMeshAgent _agent;
+    private readonly RotationController _rotationController;
+
+    private float _endTime;
+
+    public RatDeathState(Rat rat, NavMeshAgent agent, RotationController rotationController, Animator animator) : base(animator)
+    {
+        _rat = rat;
+        _agent = agent;
+        _rotationController = rotationController;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        _endTime = Time.time + KnockbackDuration;
+        _agent.ResetPath();
+        _agent.isStopped = true;
+        _rotationController.LookIn(-_rat.LastDamageDirection);
+    }
+
+    public override void Tick()
+    {
+        base.Tick();
+        if (Time.time < _endTime)
+        {
+            _rat.transform.position += _rat.LastDamageDirection * KnockbackForce * Time.deltaTime;
+        }
+    }
+
+}
